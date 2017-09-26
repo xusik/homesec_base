07a8bd08c722ad8b67521fd1204fdca958e35caa
Andriy Khusainov <xusik@users.noreply.github.com>
2017-09-26 12:19:40 +0300

    Update RF_Light_Remote_Base.cpp

--- RF_Light_Remote_Base.cpp
+++ RF_Light_Remote_Base.cpp
@@ -10,15 +10,15 @@
 // 	                               +-\/-+
 // 	                        VCC   1|    |20  GND
 // 	                 RELAY1 P1.0  2|    |19  XIN
-// 	                 UART   P1.1  3|    |18  XOUT
-// 	                 UART   P1.2  4|    |17  TEST
+// 	         ESP8266 UART   P1.1  3|    |18  XOUT
+// 	         ESP8266 UART   P1.2  4|    |17  TEST
 // 	                 BUTTON P1.3  5|    |16  RST#
 // 	                 RELAY2 P1.4  6|    |15  P1.7 MOSI  6
 // 	                 5  SCK P1.5  7|    |14  P1.6 MISO  7
 // 	                 3   CE P2.0  8|    |13  P2.5 RELAY3
-// 	   	 	         4  CSN P2.1  9|    |12  P2.4 RELAY4
-//		 	         8  IRQ P2.2 10|    |11  P2.3 BUZZER
-// 			                       +----+
+// 	   	 	 4  CSN P2.1  9|    |12  P2.4 RELAY4
+//		         8  IRQ P2.2 10|    |11  P2.3 BUZZER
+// 			               +----+
 //
 //
 
